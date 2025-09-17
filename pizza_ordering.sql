e17bbba756f63930e2a8e340d89994135cdd13dd
Noortje van Maldegem <n.vanmaldegem@student.maastrichtuniversity.nl>
2025-09-17 13:05:09 +0200

    creates .sql file

--- pizza_ordering.sql
+++ pizza_ordering.sql
@@ -1,4 +1,4 @@
--- MySQL dump 10.13  Distrib 9.4.0, for macos14.7 (arm64)
+-- MySQL dump 10.13  Distrib 9.4.0, for macos14.7 (x86_64)
 --
 -- Host: localhost    Database: pizza_ordering
 -- ------------------------------------------------------
@@ -14,29 +14,6 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `pizza`
---
-
-DROP TABLE IF EXISTS `pizza`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `pizza` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `pizza`
---
-
-LOCK TABLES `pizza` WRITE;
-/*!40000 ALTER TABLE `pizza` DISABLE KEYS */;
-/*!40000 ALTER TABLE `pizza` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -47,4 +24,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-09-16 16:25:30
+-- Dump completed on 2025-09-17 13:03:59
